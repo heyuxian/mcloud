cb56999a715e5cdb91236fadbcc00f20bee9fc37
heyuxian <heyuxian.me@gmail.com>
2017-10-12 22:28:09 +0800

    添加配置中心

--- mcloud-apigw/src/main/java/me/javaroad/config/ApiGwApplication.java
+++ mcloud-apigw/src/main/java/me/javaroad/config/ApiGwApplication.java
@@ -1,4 +1,4 @@
-package me.javaroad.apigw;
+package me.javaroad.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ mcloud-config/src/main/java/me/javaroad/config/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package me.javaroad.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author heyx
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
