7655d04e63f43f697deba000724759d192621dec
heyuxian <heyuxian.me@gmail.com>
2017-10-14 16:05:49 +0800

    Swagger UI 添加认证 OAuth

--- mcloud-web/src/main/java/me/javaroad/web/config/Swagger2Config.java
+++ mcloud-web/src/main/java/me/javaroad/web/config/Swagger2Config.java
@@ -13,16 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package me.javaroad.blog.config;
+package me.javaroad.web.config;
 
+import com.google.common.collect.Lists;
 import io.swagger.annotations.ApiOperation;
-import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.OAuth;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -36,26 +44,53 @@ public class Swagger2Config {
 
     private static final String VERSION = "1.0";
 
+    @Value("${info.app.name}")
+    private String serviceName;
+    @Value("${info.app.desc}")
+    private String serviceDesc;
+    @Value("${uaa.url:}")
+    private String oAuthServerUri;
+    @Value("${uaa.clientId:}")
+    private String clientId;
+    @Value("${uaa.clientSecret:}")
+    private String clientSecret;
+
     @Bean
     public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
             .select()
             .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
             .paths(PathSelectors.any())
             .build()
-            .directModelSubstitute(LocalDate.class, java.sql.Date.class)
-            .directModelSubstitute(LocalDate.class, java.util.Date.class)
             .apiInfo(apiInfo());
+        if(!StringUtils.isBlank(oAuthServerUri)) {
+            docket.securitySchemes(Lists.newArrayList(securitySchema()));
+        }
+        return docket;
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("Swagger API")
-            .description("MCloud-http://javaroad.me")
+            .title(serviceName)
+            .description(serviceDesc)
             .license("Apache 2.0")
             .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
             .termsOfServiceUrl("")
             .version(VERSION)
             .build();
+    }
+
+    private OAuth securitySchema() {
+        ResourceOwnerPasswordCredentialsGrant resourceOwnerPasswordCredentialsGrant =
+            new ResourceOwnerPasswordCredentialsGrant("http://localhost:8043/uaa/oauth/token");
+        List<GrantType> grantTypes = Collections.singletonList(resourceOwnerPasswordCredentialsGrant);
+        return new OAuth("oauth2", scopes(), grantTypes);
+    }
+
+    private List<AuthorizationScope> scopes() {
+        List<AuthorizationScope> list = Lists.newArrayList();
+        list.add(new AuthorizationScope("read", "Grants read access"));
+        return list;
     }
 }
