f994d24e85d697e0f65678d1f5ef9f69d54b66b0
heyuxian <heyuxian.me@gmail.com>
2018-03-06 19:42:34 +0800

    update entity

--- mcloud-message/src/main/java/me/javaroad/message/code/dto/request/SendCodeRequest.java
+++ mcloud-message/src/main/java/me/javaroad/message/code/dto/request/SendCodeRequest.java
@@ -2,12 +2,16 @@ package me.javaroad.message.code.dto.request;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import me.javaroad.message.code.entity.CodeType;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @author heyx
  */
+@Getter
+@Setter
 public class SendCodeRequest {
     @NotEmpty
     @Size(min = 6, max = 50)
